af1d1f73f6a7e05ba4a3b9c591b6b1945a0b7b57
jamestariga <jamestariga08@live.com>
2022-02-19 13:22:46 -0800

    Added user input

--- trivia.py
+++ trivia.py
@@ -1,10 +1,17 @@
+import os
+import discord
+
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+TRIVIA_API = os.getenv('TRIVIA_API')
 
 choose = input('Enter category: ')
 category = choose
 api_url = 'https://api.api-ninjas.com/v1/trivia?category={}'.format(category)
-response = requests.get(api_url, headers={'X-Api-Key': 'GTFcyQ5xs0/j6KG7rV3jzg==WJ13oCWtAGfufGYY'})
+response = requests.get(api_url, headers={'X-Api-Key': TRIVIA_API})
 if response.status_code == requests.codes.ok:
-    print(response.json())
+    print(response.json()[0]['category'])
 else:
     print("Error:", response.status_code, response.text)
