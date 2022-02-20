42a77645eadb427957953c21a475a7db42909f55
ejrey <dayrey24@gmail.com>
2022-02-19 18:26:18 -0800

    Implemented idea of putting social feature

--- main.py
+++ main.py
@@ -16,13 +16,15 @@ client = commands.Bot(command_prefix="!")
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-
 @client.command()
 async def social(ctx): # The name of the function is the name of the command
-    # print(arg) # this is the text that follows the command
-    # await ctx.send(int(arg) ** 2) # ctx.send sends text in chat
-    # channel = discord.utils.get(ctx.guild.channels, name=given_name)
+    await ctx.send(welcome_msg())
 
-    await ctx.send(social_mode())
+
+@client.command()
+async def go(ctx):
+    await ctx.send('Here is your question {}'.format(ctx.author.mention) + "\n**" + social_mode() + "**")
+
+
 
 client.run(TOKEN)
--- social.py
+++ social.py
@@ -1,12 +1,22 @@
 import random
 
+def welcome_msg():
+
+    welcome = "**SOCIAL: Get to know your fellow members!**\n" \
+              "- Type **!social me** and a question will be displayed\n" \
+              "- After answering, next user will type **!social me**\n" \
+              "- Got it? type **!go** to start"
+
+
+    return welcome
+
+
 
 def social_mode():
     with open("questions.txt") as file:
         questions_array = file.readlines()
-    return random_number(questions_array)
+    return random_question(questions_array)
 
-def random_number(arr):
+def random_question(arr):
     return random.choice(arr)
-
 
