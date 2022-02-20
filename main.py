0454ba00d51a3240522ec3e7408c3b6bc048323f
Devin Lu <ludevinl@sfu.ca>
2022-02-19 16:24:39 -0800

    added bot commands for categories and platforms

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
+from discord.ext import commands
+from dotenv import load_dotenv, find_dotenv
 import os
+import games
 
-import discord
-from dotenv import load_dotenv
+load_dotenv(find_dotenv())
 
-load_dotenv()
-TOKEN = os.getenv('TOKEN')
+API_TOKEN = os.environ.get('TOKEN')
 
-client = discord.Client()
 
-@client.event
+bot = commands.Bot(command_prefix="!")
+
+categories = ["mmorpg", "shooter", "strategy", "moba", "racing", "sports", "social", "sandbox", "open-world", "survival", "pvp", "pve", "pixel", "voxel", "zombie", "turn-based", "first-person", "third-Person", "top-down", "tank", "space", "sailing", "side-scroller", "superhero", "permadeath", "card", "battle-royale", "mmo", "mmofps", "mmotps", "3d", "2d", "anime", "fantasy", "sci-fi", "fighting", "action-rpg", "action", "military", "martial-arts", "flight", "low-spec", "tower-defense", "horror", "mmorts"]
+platforms = ["pc", "browser", "all"]
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} succesfully logged in!')
 
-client.run(TOKEN)
+@bot.command(name='game_categories')
+async def get_categories(ctx):
+    await ctx.send(f'List of categories:\n{categories}')
+
+@bot.command(name='game_platforms')
+async def get_platforms(ctx):
+    await ctx.send(f'List of platforms:\n{platforms}')
+
+@bot.command(name='recommend_game')
+async def recc_game(ctx, args1=None, args2=None):
+    if args1 is not None and not isinstance(args1, str):
+        await ctx.send(f'Sorry, {args1} is not a valid command!')
+    
+    if args2 is not None and not isinstance(args2, str):
+        await ctx.send(f'Apologies, {args2} is not a valid command!')
+    
+    await ctx.send(f'Completed {args1} {args2}')
+
+bot.run(API_TOKEN)
